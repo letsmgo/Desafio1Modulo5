d2d762947b5d73fc7e26b89447f165c468a856ed
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-10 16:29:35 -0300

    Criar classe ContaController

---
+++ src/main/java/br/com/zup/GerenciadorDeContas/GerenciarContas/ContaController.java
@@ -0,0 +1,19 @@
+package br.com.zup.GerenciadorDeContas.GerenciarContas;
+
+
+import br.com.zup.GerenciadorDeContas.GerenciarContas.Dtos.ContaDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+    @Autowired
+    private ContaService contaService;
+
+
+
+
+}
