78dcf13b36c388ae0fd28a2a4e4ea6effdf36ec3
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-10 15:52:12 -0300

    Criar interface ContaRepository

---
+++ src/main/java/br/com/zup/GerenciadorDeContas/GerenciarContas/ContaRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.GerenciadorDeContas.GerenciarContas;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContaRepository extends CrudRepository<Conta, String> {
+
+}
