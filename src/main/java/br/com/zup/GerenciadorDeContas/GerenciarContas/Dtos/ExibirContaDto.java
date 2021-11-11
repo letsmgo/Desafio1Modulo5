2b6bf5950b53c46a932a3551369aff62b429bc86
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-11 13:48:31 -0300

    Criar DTO de retorno de informações sobre contas

---
+++ src/main/java/br/com/zup/GerenciadorDeContas/GerenciarContas/Dtos/ExibirContaDto.java
@@ -0,0 +1,45 @@
+package br.com.zup.GerenciadorDeContas.GerenciarContas.Dtos;
+
+import br.com.zup.GerenciadorDeContas.GerenciarContas.Enums.StatusDaConta;
+
+public class ExibirContaDto {
+    private Integer id;
+    private String nome;
+    private Double valor;
+    private StatusDaConta statusDaConta;
+
+    public ExibirContaDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public StatusDaConta getStatusDaConta() {
+        return statusDaConta;
+    }
+
+    public void setStatusDaConta(StatusDaConta statusDaConta) {
+        this.statusDaConta = statusDaConta;
+    }
+}
