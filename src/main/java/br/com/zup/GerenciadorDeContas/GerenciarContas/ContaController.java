6cd8d0c4a65a87a6a4c42cee916c4d8963999062
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-11 13:53:45 -0300

    Adicionar método de exibição de todas as contas na controller

--- src/main/java/br/com/zup/GerenciadorDeContas/GerenciarContas/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/GerenciarContas/ContaController.java
@@ -2,9 +2,14 @@ package br.com.zup.GerenciadorDeContas.GerenciarContas;
 
 
 import br.com.zup.GerenciadorDeContas.GerenciarContas.Dtos.ContaDTO;
+import br.com.zup.GerenciadorDeContas.GerenciarContas.Dtos.ExibirContaDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/contas")
@@ -12,12 +17,27 @@ public class ContaController {
 
     @Autowired
     private ContaService contaService;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Conta incluirConta(@RequestBody ContaDTO contaDTO){
 
         return contaService.incluirConta(contaDTO);
+    }
+
+    @GetMapping
+    public List<ExibirContaDto> exibirContas(@RequestParam(required = false) Integer id,
+                                                @RequestParam(required = false) String nome,
+                                                @RequestParam(required = false) Double valor){
+        List<ExibirContaDto> contaDtos = new ArrayList<>();
+        for (Conta conta : contaService.exibirContasCadastradas(id,nome,valor)) {
+            ExibirContaDto exibirContaDto = modelMapper.map(conta, ExibirContaDto.class);
+
+            contaDtos.add(exibirContaDto);
+        }
+        return contaDtos;
     }
 
 
