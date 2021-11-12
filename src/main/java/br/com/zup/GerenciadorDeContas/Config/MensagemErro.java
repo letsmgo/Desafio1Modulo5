d0b5eef29fdc4aa807b1e03ea9da7020b6bd78ac
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-11 23:22:13 -0300

    Criar classe MensagemErro

---
+++ src/main/java/br/com/zup/GerenciadorDeContas/Config/MensagemErro.java
@@ -0,0 +1,19 @@
+package br.com.zup.GerenciadorDeContas.Config;
+
+public class MensagemErro {
+    private String mensagemErro;
+
+    public MensagemErro(String mensagemErro) {
+        this.mensagemErro = mensagemErro;
+    }
+
+    public String getMensagemErro() {
+        return mensagemErro;
+    }
+
+    public void setMensagemErro(String mensagemErro) {
+        this.mensagemErro = mensagemErro;
+    }
+
+}
+
