aaaf09cd6b52748418ef10443f22dfe1efe157d4
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-11 23:22:34 -0300

    Criar classe ErroValidacao

---
+++ src/main/java/br/com/zup/GerenciadorDeContas/Config/ErroValidacao.java
@@ -0,0 +1,27 @@
+package br.com.zup.GerenciadorDeContas.Config;
+
+public class ErroValidacao {
+    private String campo;
+    private String mensagem;
+
+    public ErroValidacao(String mensagem, String campo) {
+        this.mensagem = mensagem;
+        this.campo = campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+}
