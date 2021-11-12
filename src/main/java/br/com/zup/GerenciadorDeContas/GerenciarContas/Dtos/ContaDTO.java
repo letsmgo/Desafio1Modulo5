f74be2da40b7eea825416122a867927f48f5fada
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-11 23:15:46 -0300

    Adicionar anotações de validação

--- src/main/java/br/com/zup/GerenciadorDeContas/GerenciarContas/Dtos/ContaDTO.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/GerenciarContas/Dtos/ContaDTO.java
@@ -2,13 +2,29 @@ package br.com.zup.GerenciadorDeContas.GerenciarContas.Dtos;
 
 import br.com.zup.GerenciadorDeContas.GerenciarContas.Enums.TipoDeConta;
 
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class ContaDTO {
-
+    @Enumerated(EnumType.STRING)
+    @NotNull
     private TipoDeConta tipoDeConta;
+
+    @Column(nullable = false)
+    @Size(min = 2, message = "Digite um nome válido")
     private String nome;
+
+    @DecimalMin(value = "0.1", inclusive = false, message = "Valor incompleto")
+    @Digits(integer = 10, fraction = 2, message = "Valor inválido")
     private double valor;
+
+    @NotNull(message = "Data de vencimento inválida")
     private LocalDate dataDeVencimento;
 
     public ContaDTO() {
